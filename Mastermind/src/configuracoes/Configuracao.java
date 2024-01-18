753634a98698fddf168094130204d75863ade5df
Tiago Senger dos Santos <tiagosengerds@gmail.com>
2024-01-18 16:34:48 -0300

    update finalização aula 18.01

--- Mastermind/src/configuracoes/Configuracao.java
+++ Mastermind/src/configuracoes/Configuracao.java
@@ -1,14 +1,17 @@
 package configuracoes;
 
 public class Configuracao {
-	private String alfabeto;
-	
-	public String getAlfabeto() {
-		return alfabeto;
-	}
-	
-	public void setAlfabeto (String alfabeto) {
-		this.alfabeto = alfabeto;
-	}
-	
+    private String alfabeto;
+
+    public String getAlfabeto() {
+        return alfabeto;
+    }
+
+    public void setAlfabeto(String alfabeto) {
+        if (alfabeto == null || alfabeto.length() < 1) {
+            throw new IllegalArgumentException("O alfabeto deve possuir mais de 1 caracter");
+        }
+        this.alfabeto = alfabeto;
+    }
 }
+
--- Mastermind/src/configuracoes/TestConfiguracao.java
+++ Mastermind/src/configuracoes/TestConfiguracao.java
@@ -2,16 +2,33 @@ package configuracoes;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 import org.junit.jupiter.api.Test;
 
 class TestConfiguracao {
 
 	@Test
-	void test() {
+	void testSetAlfabeto() {
 		Configuracao configuracao = new Configuracao();
+		
 		String alfabeto = "ABCDEFGH";
-		configuracao.setAlfabeto(alfabeto);
+		
+		try {
+			configuracao.setAlfabeto(alfabeto);			
+		} catch (Exception e1) {
+			fail("Gerou exceção indevida");
+			e1.printStackTrace();
+		}
 		assertEquals(alfabeto, configuracao.getAlfabeto());
+		
+		alfabeto = null;
+		
+		try {
+			configuracao.setAlfabeto(alfabeto);
+		} catch (Exception e) {
+			assertEquals("O alfabeto deve possuir mais de 1 caracter", e.getMessage());
+		}
+		assertFalse(configuracao.getAlfabeto()==null);
 	}
 
 }
