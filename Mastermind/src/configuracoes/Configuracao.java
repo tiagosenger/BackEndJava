7270219b6a5768096dce81f87fb30e6cebe35f9d
Tiago Senger dos Santos <tiagosengerds@gmail.com>
2024-01-18 15:54:49 -0300

    update exercicios da aula 18.01

---
+++ Mastermind/src/configuracoes/Configuracao.java
@@ -0,0 +1,14 @@
+package configuracoes;
+
+public class Configuracao {
+	private String alfabeto;
+	
+	public String getAlfabeto() {
+		return null;
+	}
+	
+	public void setAlfabeto (String alfabeto) {
+		
+	}
+	
+}
